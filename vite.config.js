d8b70c4994f34efafc1ee1db6a97bc074d77dfee
Rayulu Mukku <dasaradharam109@gmail.com>
2026-08-16 19:23:07 +0530

    update config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/project/code-snippet-manager/',
   optimizeDeps: {
     include: ['react-syntax-highlighter', 'react-syntax-highlighter/dist/esm/styles/prism'],
   },
